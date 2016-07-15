f9b1d6cf78699412319d0ffca99e13b0e5cba498
Markus Rathgeb <maggu2810@gmail.com>
2016-07-15 14:58:02 +0200

    change index creation script
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bndtools/cnf/NOTES.md
+++
@@ -1,3 +0,0 @@
-Fix _remote.properties
-
-mvn clean install 2>&1 | grep 'java.lang.IllegalArgumentException' | grep "${HOME}/.m2/repository" | cut -d\= -f2- | cut -d\  -f5- | grep "^${HOME}/.m2/repository" | while read LINE; do DIR="$(dirname "${LINE}")"; rm -vrf "${DIR}"; done
---
+++ bndtools/cnf/index.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TMP_DIR="`mktemp -d`"
+
+TMP_STDERR="${TMP_DIR}"/stderr
+TMP_IAE="${TMP_DIR}"/iae
+TMP_IAE_REPO="${TMP_DIR}"/iae_repo
+TMP_ARTIFACTS="${TMP_DIR}"/artifacts
+loop=1
+while [ ${loop} = 1 ]
+do
+  mvn -am -pl tools/shk-deps/ clean install 2> "${TMP_STDERR}"
+  grep 'java.lang.IllegalArgumentException' "${TMP_STDERR}" > "${TMP_IAE}"
+  grep "${HOME}/.m2/repository" "${TMP_IAE}" > "${TMP_IAE_REPO}"
+  cat "${TMP_IAE_REPO}" | cut -d\= -f2- | cut -d\  -f5- | grep "^${HOME}/.m2/repository" > "${TMP_ARTIFACTS}"
+  grep "^${HOME}/.m2/repository" "${TMP_ARTIFACTS}" || exit
+  cat "${TMP_ARTIFACTS}" | while read LINE; do DIR="$(dirname "${LINE}")"; rm -vrf "${DIR}"; done
+done
+
