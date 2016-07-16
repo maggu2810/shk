c6e5fc908be25698a7e3e1cb6019b49dfc2a8d6a
Markus Rathgeb <maggu2810@gmail.com>
2016-07-16 20:23:47 +0200

    remove obsolete files
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bndtools/cnf/index.sh
+++ bndtools/cnf/index.sh
@@ -9,7 +9,8 @@ TMP_ARTIFACTS="${TMP_DIR}"/artifacts
 loop=1
 while [ ${loop} = 1 ]
 do
-  mvn -am -pl tools/shk-deps/ clean install 2> "${TMP_STDERR}"
+  #mvn -am -pl tools/shk-deps/ clean install 2> "${TMP_STDERR}"
+  mvn clean install 2> "${TMP_STDERR}"
   grep 'java.lang.IllegalArgumentException' "${TMP_STDERR}" > "${TMP_IAE}"
   grep "${HOME}/.m2/repository" "${TMP_IAE}" > "${TMP_IAE_REPO}"
   cat "${TMP_IAE_REPO}" | cut -d\= -f2- | cut -d\  -f5- | grep "^${HOME}/.m2/repository" > "${TMP_ARTIFACTS}"
