a3b77f7b6aeca952805f8b0f05d5451d3bcf0137
Markus Rathgeb <maggu2810@gmail.com>
2016-10-24 13:40:34 +0200

    use license header of upstream project
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/shk-library-chromecast-api/src/main/java/su/litvak/chromecast/api/v2/Status.java
+++ bundles/shk-library-chromecast-api/src/main/java/su/litvak/chromecast/api/v2/Status.java
@@ -1,9 +1,6 @@
 /*
- * #%L
- * shk :: Bundles :: Library :: Chromecast API
- * %%
- * Copyright (C) 2014 Vitaly Litvak (vitavaque@gmail.com) and others.
- * %%
+ * Copyright 2014 Vitaly Litvak (vitavaque@gmail.com)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * #L%
  */
 
 package su.litvak.chromecast.api.v2;
