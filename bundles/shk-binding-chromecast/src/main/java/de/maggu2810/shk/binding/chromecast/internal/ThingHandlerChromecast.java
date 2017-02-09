8f27d065b1a9a06244685c3570dcad487875c836
Markus Rathgeb <maggu2810@gmail.com>
2017-02-09 18:37:23 +0100

    update license header
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/shk-binding-chromecast/src/main/java/de/maggu2810/shk/binding/chromecast/internal/ThingHandlerChromecast.java
+++ bundles/shk-binding-chromecast/src/main/java/de/maggu2810/shk/binding/chromecast/internal/ThingHandlerChromecast.java
@@ -2,7 +2,7 @@
  * #%L
  * shk :: Bundles :: Binding :: Chromecast
  * %%
- * Copyright (C) 2015 - 2016 maggu2810
+ * Copyright (C) 2015 - 2017 maggu2810
  * %%
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
--- bundles/shk-io-rest-docs/src/main/java/de/maggu2810/shk/io/rest/docs/internal/RestDocs.java
+++ bundles/shk-io-rest-docs/src/main/java/de/maggu2810/shk/io/rest/docs/internal/RestDocs.java
@@ -2,7 +2,7 @@
  * #%L
  * shk :: Bundles :: IO :: REST Docs
  * %%
- * Copyright (C) 2015 - 2016 maggu2810
+ * Copyright (C) 2015 - 2017 maggu2810
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
