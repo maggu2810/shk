6f5da50d9934f7a0f4c772cb929c025f8a22aa2a
Markus Rathgeb <maggu2810@gmail.com>
2018-11-20 15:47:18 +0100

    REST doc: fix header
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/shk-io-rest-docs/src/main/java/de/maggu2810/shk/io/rest/docs/internal/RestDocs.java
+++ bundles/shk-io-rest-docs/src/main/java/de/maggu2810/shk/io/rest/docs/internal/RestDocs.java
@@ -1,8 +1,8 @@
 /*-
  * #%L
- * shk :: Bundles :: IO :: REST Docs
+ * shk - Bundles - IO - REST Docs
  * %%
- * Copyright (C) 2015 - 2017 maggu2810
+ * Copyright (C) 2015 - 2018 maggu2810
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
